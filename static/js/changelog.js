6443b9624219d6e935d64cb35a290cac61d73ae5
moscoso <christophermoscoso@gmail.com>
2018-05-03 18:44:42 -0400

    Smoother collapsible animation

--- static/js/changelog.js
+++ static/js/changelog.js
@@ -5,26 +5,28 @@ $(document).ready(() => {
             let version = $(changeSet).attr('version');
             let $changeSetTemplate = $(`<div class="changeset card">
             </div>`);
-            let $headerTemplate = $(`<div class="card-header"><button class="btn btn-link" data-toggle="collapse" data-target="#collapse${index}" aria-controls="collapse${index}">Version: ${version}</button></div>`);
-            let $itemsTemplate = $(`<div id="collapse${index}" class="collapse card-body" data-parent="#changelog"</div>`);
+            let $headerTemplate = $(`<div class="card-header"><button class="btn btn-link" data-toggle="collapse" data-target="#collapse${index}">Version: ${version}</button></div>`);
+            let $itemsTemplate = $(`<div id="collapse${index}" class="collapse"><div class="card-body"></div></div>`);
             if(index == 0){
                 $itemsTemplate.addClass('show');
             }
             $(changeSet).find('item').each( (index, element)  => {
                 let item = $(element).attr('text');
                 let $itemTemplate = $(`<li class="item">${item}</li>`);
-                $itemsTemplate.append($itemTemplate)
+                $itemsTemplate.find('.card-body').append($itemTemplate);
             });
             $changeSetTemplate.append($headerTemplate);
             $changeSetTemplate.append($itemsTemplate);
             $('#changelog').append($changeSetTemplate);
         });   
-        $('#showAllChangeSetsBtn').click(() => {
-            $('.collapse').collapse('show');
-        });
+        
+    });
 
-        $('#hideAllChangeSetsBtn').click(() => {
-            $('.collapse').collapse('hide');
-        });
+    $('#showAllChangeSetsBtn').click(() => {
+        $('.collapse').addClass('show');
+    });
+
+    $('#hideAllChangeSetsBtn').click(() => {
+        $('.collapse').removeClass('show');
     });
 });
